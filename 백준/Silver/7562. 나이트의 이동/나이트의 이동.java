ae96d497c83b30095d7ffae26fecca0c29a8ce12
cleveryellowduck <61223304+jeongabae@users.noreply.github.com>
2025-02-14 23:19:44 +0900

    [Silver I] Title: 나이트의 이동, Time: 296 ms, Memory: 51048 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
@@ -2,63 +2,58 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	
+
 	static int l;
 	static int[][] arr;
-	static int[][] visited;
-	static int[] dr = {-2, -2, -1, -1, 1, 1, 2, 2};
-	static int[] dc = {-1, 1, -2, 2, -2, 2, -1, 1};
-	public static void main(String[] args) throws Exception{
+	static int[] dr = { -2, -2, -1, -1, 1, 1, 2, 2 };
+	static int[] dc = { -1, 1, -2, 2, -2, 2, -1, 1 };
+
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		
-		//0. 테스트 케이스 개수
+
 		int T = Integer.parseInt(br.readLine());
-		for(int tc=1;tc<=T;tc++) {
-		//1. 체스판 한 변의 길이 l
-		l = Integer.parseInt(br.readLine());
-		
-		//arr, visited
-		arr = new int[l][l];
-		visited = new int[l][l];
-		
-		//나이트가 현재 있는 칸
-		st = new StringTokenizer(br.readLine());
-		Point s = new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
-		
-        //3. 나이트가 이동하려고 하는 칸 
-		st = new StringTokenizer(br.readLine());
-		Point e = new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
-		
-		Queue<Point> q = new LinkedList<>();
-		q.add(s);
-		visited[s.r][s.c] = 1;
-		while(!q.isEmpty()) {
-			Point now = q.poll();
-			if (now.r == e.r && now.c == e.c) break; //찾음
-			for(int i=0;i<8;i++) {
-				int nr = now.r + dr[i];
-				int nc = now.c + dc[i];
-				if (nc < 0 || nc >= l || nr < 0 || nr >= l) //범위 밖에 넘어간 경우 처리 x
-					continue;
-				if (visited[nr][nc] == 0) {  //방문하지 않은 경우만 +1
-					visited[nr][nc] = visited[now.r][now.c] + 1;
-					q.add(new Point(nr, nc));
-			}
+		for (int tc = 1; tc <= T; tc++) {
+			l = Integer.parseInt(br.readLine());
+			arr = new int[l][l];
+
+			st = new StringTokenizer(br.readLine());
+			int sX = Integer.parseInt(st.nextToken());
+			int sY= Integer.parseInt(st.nextToken());
+            
+			st = new StringTokenizer(br.readLine());
+			int eX = Integer.parseInt(st.nextToken());
+			int eY= Integer.parseInt(st.nextToken());
+
 			
-		}
-		}
-		System.out.println(visited[e.r][e.c]-1);
-	}
+			ArrayDeque<int[]> q = new ArrayDeque<>();
+			q.offer(new int[]{sX, sY});
+			arr[sX][sY]=1;
+			
+			while (!q.isEmpty()) {
+				int[] now = q.poll();
+				int nowX = now[0];
+				int nowY = now[1];
+				
+				if (nowX == eX && nowY == eY)
+					break;
+				
+				for (int i = 0; i < 8; i++) {
+					int nr = nowX + dr[i];
+					int nc = nowY + dc[i];
+					
+					if (nc < 0 || nc >= l || nr < 0 || nr >= l)
+						continue;
+					
+					if (arr[nr][nc] == 0) {
+						arr[nr][nc] = arr[nowX][nowY] + 1;
+						q.offer(new int[] {nr, nc});
+					}
 
-}
-}
+				}
+			}
+			System.out.println(arr[eX][eY]-1);
+		}
 
-class Point{
-	int r;
-	int c;
-	Point(int r, int c) {
-		this.r=r;
-		this.c=c;
 	}
 }
