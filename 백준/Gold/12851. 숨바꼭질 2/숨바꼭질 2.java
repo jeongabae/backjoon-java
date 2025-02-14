33f6945de4acf8af102c9dfb2fe98a3a27128473
cleveryellowduck <61223304+jeongabae@users.noreply.github.com>
2025-02-14 17:00:23 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 144 ms, Memory: 21664 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
@@ -3,41 +3,39 @@ import java.util.*;
 
 public class Main {
 	static int n, k;
-	static int[] arr = new int[100001];;
-	static int[] visited = new int[100001];
-	static int[] count = new int[100001];
-	public static void main(String[] args) throws Exception{
+	static int[] arr = new int[100001];
+	static int[] cnt = new int[100001];
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		n = Integer.parseInt(st.nextToken());
 		k = Integer.parseInt(st.nextToken());
-		
-		Queue<Integer> q = new LinkedList<>(); //큐
-        q.add(n);//큐에 수빈이 위치 넣고
-        visited[n] = 1; //방문
-        count[n] = 1; 
-        
-        while (!q.isEmpty()) { //큐가 비어있지 않으면 반복
-            int now = q.poll(); //큐에서 하나 꺼냄
 
-            if (now == k) break; //동생 만나면 멈춤
-            int[] next = {now - 1, now + 1, now * 2}; //갈 수 있는거 세 가지
-            for (int i = 0; i < 3; i++) { //세 가지 경우 해봄
-                if(!isRange(next[i])) continue; //범위 벗어나면 continue
-                if (visited[next[i]] == 0) { //방문하지 않았으면 방문
-                    visited[next[i]] = visited[now] + 1;
-                    count[next[i]] = count[now];
-                    q.add(next[i]); //큐에 넣음
-                } else if (visited[next[i]] == visited[now] + 1) {
-                    count[next[i]] += count[now];
-                }
-            }
-        }
-        System.out.println(visited[k] - 1);
-        System.out.println(count[k]);
-    }
-    static boolean isRange(int x) {
-        return x >= 0 && x <= 100000;
-    }
+		ArrayDeque<Integer> q = new ArrayDeque<>();
+		q.offer(n);
+		arr[n] = 1;
+		cnt[n] = 1;
+		while (!q.isEmpty()) {
+			int now = q.poll();
+			if (now == k)
+				break;
+			int[] can_next = { now - 1, now + 1, now * 2 };
+			for (int i = 0; i < 3; i++) {
+				int next = can_next[i];
+				if(!(0<=next&&next<100001))
+					continue;
+				if (arr[next] == 0) {
+					arr[next] = arr[now] + 1;
+					q.offer(next);
+					cnt[next] = cnt[now];
+				}else if(arr[next]==arr[now]+1) {
+					cnt[next] += cnt[now];
+				}
+			}
+		}
+		
+		System.out.println(arr[k]-1);
+		System.out.println(cnt[k]);
+	}
 
 }
