0648349bbd90fba41acfeb1eb052968b2742bba3
cleveryellowduck <61223304+jeongabae@users.noreply.github.com>
2025-02-22 22:38:44 +0900

    [Silver IV] Title: 수 찾기, Time: 1116 ms, Memory: 53656 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main (String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(arr);
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        while (M-- > 0) {
+            int x = Integer.parseInt(st.nextToken());
+             int idx = Arrays.binarySearch(arr, x);
+             System.out.println(idx >= 0 ? 1 : 0);
+        }
+    }
+}
