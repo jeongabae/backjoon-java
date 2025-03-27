630e568da1fa254f7b1f8de4739e062cde50ebc1
cleveryellowduck <61223304+jeongabae@users.noreply.github.com>
2025-03-27 21:47:12 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 556 ms, Memory: 49336 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -0,0 +1,76 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static class Edge implements Comparable<Edge> {
+        int from, to, weight;
+
+        public Edge(int from, int to, int weight) {
+            this.from = from;
+            this.to = to;
+            this.weight = weight;
+        }
+
+        @Override
+        public int compareTo(Edge o) {
+            return Integer.compare(this.weight, o.weight);
+        }
+    }
+
+    static Edge[] edgeList;
+    static int[] parents;
+    static int V, E;
+
+    static void make() {
+        parents = new int[V + 1];
+        for (int i = 1; i <= V; i++) {
+            parents[i] = i;
+        }
+    }
+
+    static int find(int a) {
+        if (a == parents[a]) return a;
+        return parents[a] = find(parents[a]);
+    }
+
+    static boolean union(int a, int b) {
+        int aRoot = find(a);
+        int bRoot = find(b);
+        if (aRoot == bRoot) return false;
+
+        parents[bRoot] = aRoot;
+        return true;
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        V = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+
+        edgeList = new Edge[E];
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+            edgeList[i] = new Edge(from, to, weight);
+        }
+
+        Arrays.sort(edgeList);
+        make();
+
+        long result = 0;
+        int count = 0;
+
+        for (Edge edge : edgeList) {
+            if (union(edge.from, edge.to)) {
+                result += edge.weight;
+                if (++count == V - 1) break;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
