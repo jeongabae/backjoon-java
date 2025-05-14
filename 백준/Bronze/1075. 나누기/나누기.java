5c6d5145d43ba34d1802960722ee2fde8a249dc4
cleveryellowduck <61223304+jeongabae@users.noreply.github.com>
2025-05-14 23:22:18 +0900

    [Bronze II] Title: 나누기, Time: 68 ms, Memory: 11768 KB -BaekjoonHub

---
+++ 백준/Bronze/1075. 나누기/나누기.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int F = Integer.parseInt(br.readLine());
+
+        int base = (N/100) * 100;
+
+        for (int i = 0; i < 100; i++) {
+            if ((base + i) % F == 0) {
+                System.out.printf("%02d\n", i);
+                break;
+            }
+        }
+    }
+}
