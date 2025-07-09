fa5c5e62db95436d6619ad819462fee6ab92cbae
cleveryellowduck <61223304+jeongabae@users.noreply.github.com>
2025-07-09 17:22:18 +0900

    [Gold III] Title: 중량제한, Time: 420 ms, Memory: 60340 KB -BaekjoonHub

--- 백준/Gold/1939. 중량제한/중량제한.java
+++ 백준/Gold/1939. 중량제한/중량제한.java
@@ -2,66 +2,70 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[] parent;
+    static List<Node>[] graph;
+    static boolean[] v;
 
-    static class Node implements Comparable<Node>{
-        int from, to;
+    static class Node implements Comparable<Node> {
+        int to;
         long weight;
-        Node(int from,  int to, long weight){
-            this.from=from;
-            this.to=to;
-            this.weight=weight;
+
+        Node(int to, long weight) {
+            this.to = to;
+            this.weight = weight;
         }
 
-        public int compareTo(Node o){
-            return Long.compare(o.weight,this.weight);
+        public int compareTo(Node o) {
+            return Long.compare(o.weight, this.weight);
         }
-    }
-    static int find(int x){
-        if(parent[x]!=x){
-            parent[x] = find(parent[x]);
-        }
-        return parent[x];
-    }
-
-    static void union(int a, int b) {
-        int rootA = find(a);
-        int rootB = find(b);
-        if (rootA == rootB) return;
-        parent[rootB] = rootA;
-
     }
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken()); // 섬 개수
-        int M = Integer.parseInt(st.nextToken()); // 다리 개수
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-        parent = new int[N + 1];
+        graph = new ArrayList[N+1];
+        v = new boolean[N+1];
+        
         for (int i = 1; i <= N; i++) {
-            parent[i] = i;
+            graph[i] = new ArrayList<>();
         }
-        List<Node> graph = new ArrayList<>();
+
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             long C = Integer.parseInt(st.nextToken());
-            graph.add(new Node(a, b, C));
+            graph[a].add(new Node(b, C));
+            graph[b].add(new Node(a, C));
         }
-
-        Collections.sort(graph);
 
         st = new StringTokenizer(br.readLine());
         int s = Integer.parseInt(st.nextToken());
         int e = Integer.parseInt(st.nextToken());
-        for (Node i : graph) {
-            union(i.from, i.to);
-            if(find(s)==find(e)){
-                System.out.println(i.weight);
+
+        Queue<Node> pq = new PriorityQueue<>();
+        pq.offer(new Node(s, Long.MAX_VALUE));
+
+        while (!pq.isEmpty()) {
+            Node node = pq.poll();
+            int cur = node.to;
+            long w = node.weight;
+
+            if (cur == e) {
+                System.out.println(w);
                 return;
+            }
+
+            if (!v[cur]) {
+                v[cur] = true;
+                for (Node next : graph[cur]) {
+                    if (!v[next.to]) {
+                        pq.offer(new Node(next.to, Math.min(w, next.weight)));
+                    }
+                }
             }
         }
     }
