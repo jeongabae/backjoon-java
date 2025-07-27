7c24d873b9a5c63e316d6d8eb49eea11563321b7
cleveryellowduck <61223304+jeongabae@users.noreply.github.com>
2025-07-27 22:07:56 +0900

    [level 3] Title: 네트워크, Time: 0.32 ms, Memory: 82.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=java) 
 
 ### 성능 요약
 
-메모리: 89.9 MB, 시간: 0.55 ms
+메모리: 82.2 MB, 시간: 0.32 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 23일 02:31:31
+2025년 07월 27일 22:07:54
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,27 +1,30 @@
 class Solution {
     boolean[] v;
-    int cnt;
-    
+    int[][] computers;
+
     public int solution(int n, int[][] computers) {
         v = new boolean[n];
-        cnt = 0;
-        for(int i=0;i<n;i++){
-                if(!v[i]){
-                    dfs(i, computers);
-                    cnt++;
-                }
-                
-            }
-        return cnt;
-    }
-    
-    void dfs(int node, int[][] computers){
-        v[node] = true;
-        
-        for(int j=0;j<computers.length;j++){
-            if(node!=j&&computers[node][j]==1&&!v[j]){
-                dfs(j,computers);
+        this.computers = computers;
+
+        int cnt = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (!v[i]) {
+                dfs(i);
+                cnt++;
             }
         }
-    }    
+
+        return cnt;
+    }
+
+    void dfs(int node) {
+        v[node] = true;
+
+        for (int j = 0; j < computers.length; j++) {
+            if (computers[node][j] == 1 && !v[j]) {
+                dfs(j);
+            }
+        }
+    }
 }
