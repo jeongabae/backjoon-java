8c921da00dc1a88d865060a804bd61a2c154806d
cleveryellowduck <61223304+jeongabae@users.noreply.github.com>
2026-01-07 23:37:48 +0900

    [level 2] Title: 모음 사전, Time: 0.04 ms, Memory: 83.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(String word) {
+        int[] weight = {781, 156, 31, 6, 1};
+        String vowels = "AEIOU";
+
+        int ans = 0;
+
+        for (int i = 0; i < word.length(); i++) {
+            int idx = vowels.indexOf(word.charAt(i));
+            ans += idx * weight[i] + 1;
+        }
+
+        return ans;
+    }
+}
